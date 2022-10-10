f48c9e2eb17f0d14c962bea1f042b4c61fda7281
ogullcankara <ogullcankara@gmail.com>
2022-10-11 00:56:51 +0300

    Added views

--- SpotifyConfigurationScreen/Factories.swift
+++ SpotifyConfigurationScreen/Factories.swift
@@ -11,7 +11,7 @@ func makeLabel(withText text:String) -> UILabel{
     let label = UILabel()
     label.translatesAutoresizingMaskIntoConstraints = false
     label.text = text
-    label.textColor = .white
+    label.textColor = .black
     label.font = UIFont.systemFont(ofSize: 17)
     
     return label
@@ -44,3 +44,46 @@ func makeSwitch(isOn : Bool) -> UISwitch {
 
         return theSwitch
 }
+
+func makeProgressView() -> UIProgressView {
+    let progressView = UIProgressView(progressViewStyle: .default)
+    progressView.translatesAutoresizingMaskIntoConstraints = false
+    progressView.tintColor = .gray
+
+    return progressView
+}
+
+func makeStackView(withOrientation axis: NSLayoutConstraint.Axis) -> UIStackView {
+    let stackView = UIStackView()
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.axis = axis
+    stackView.distribution = .fill
+    stackView.alignment = .fill
+    stackView.spacing = 8.0
+
+    return stackView
+}
+
+public extension NSLayoutConstraint {
+    @objc func setActiveBreakable(priority: UILayoutPriority = UILayoutPriority(900)) {
+        self.priority = priority
+        isActive = true
+    }
+}
+
+extension UINavigationController {
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+extension UIColor {
+    static let darkBlue = UIColor(red: 10/255, green: 132/255, blue: 255/255, alpha: 1)
+    static let darkGreen = UIColor(red: 48/255, green: 209/255, blue: 88/255, alpha: 1)
+    static let darkOrange = UIColor(red: 255/255, green: 149/255, blue: 0/255, alpha: 1)
+    static let darkRed = UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1)
+    static let darkTeal = UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 1)
+    static let darkYellow = UIColor(red: 255/255, green: 204/255, blue: 0/255, alpha: 1)
+    static let offBlack = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+}
+
